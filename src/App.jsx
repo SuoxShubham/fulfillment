42644ea7731397947176dc06bc4928343f3c8faf
Juan <juan.puerta@macys.com>
2023-07-12 10:54:06 -0500

    feat(fulfilment): clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "container/Header";
-import Footer from "container/Footer";
 
 import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header/>
     <div>Fulfillment</div>
-    <Footer />
   </div>
 );
-ReactDOM.render(<App />, document.getElementById("app"));
+export default App
+// ReactDOM.render(<App />, document.getElementById("app"));
